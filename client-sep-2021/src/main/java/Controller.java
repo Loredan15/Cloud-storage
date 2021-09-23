d9d5ba275d38e67b25632f98b6274c6bcd93ad5b
m.shelepov <graf_ice@mail.ru>
2021-09-23 09:18:20 +0300

    HomeWork-1
    Честно, практически 1 в 1 переписал вариант, показанный на 2 занятии

--- client-sep-2021/src/main/java/Controller.java
+++ client-sep-2021/src/main/java/Controller.java
@@ -1,9 +1,14 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -13,27 +18,49 @@ import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 public class Controller implements Initializable {
 
+    private static String ROOT_DIR = "client-sep-2021/root";
+    private static byte[] buffer = new byte[1024];
     public ListView<String> listView;
     public TextField input;
     private DataInputStream is;
     private DataOutputStream os;
 
     public void send(ActionEvent actionEvent) throws Exception {
-        String msg = input.getText();
+        String fileName = input.getText();
         input.clear();
-        os.writeUTF(msg);
+        sendFile(fileName);
+    }
+
+    @SneakyThrows
+    private void sendFile(String fileName) {
+        Path file = Paths.get(ROOT_DIR, fileName);
+        if (Files.exists(file)){
+            long size = Files.size(file);
+            os.writeUTF(fileName);
+            os.writeLong(size);
+
+            InputStream fileStream = Files.newInputStream(file);
+            int read;
+            while ((read = fileStream.read(buffer)) !=-1){
+                os.write(buffer, 0, read);
+            }
+        }else{
+            os.writeUTF(fileName);
+        }
         os.flush();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            fillFilesInCurrentDir();
             Socket socket = new Socket("localhost", 8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
@@ -53,5 +80,18 @@ public class Controller implements Initializable {
         } catch (IOException ioException) {
             log.error("e=", ioException);
         }
+    }
+
+    @SneakyThrows
+    private void fillFilesInCurrentDir() {
+        listView.getItems().clear();
+        //Собственно, сама магия с получением списка папок
+        listView.getItems().addAll(Files.list(Paths.get(ROOT_DIR)).map(p -> p.getFileName().toString()).collect(Collectors.toList()));
+        listView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2){
+                String item = listView.getSelectionModel().getSelectedItem();
+                input.setText(item);
+            }
+        });
     }
 }
